6f637b95963e24221ad7778632331fbcf724b759
vy-b <kvy.bui@gmail.com>
2022-08-15 20:45:20 -0700

    firebase connect database

--- public/index.js
+++ public/index.js
@@ -1,7 +1,3 @@
-var enterButton = document.getElementById("enter");
-var input = document.getElementById("userInput");
-var ul = document.querySelector("ul");
-var item = document.getElementsByTagName("li");
 
 function inputLength(){
 	return input.value.length;
