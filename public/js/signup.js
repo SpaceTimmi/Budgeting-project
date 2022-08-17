71c1036e9863f68035ce59b0c55f1a0cb7659364
Timi Ombe <timmyombe@gmail.com>
2022-08-17 22:44:03 +0100

    Added authentication for signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,18 +1,41 @@
-// code from Firebase Docs
 
-// let email = "janedoe@gmail.com";
-// let password = "12345678";
-// const auth = firebase.auth();
-// firebase.auth().createUserWithEmailAndPassword(email, password)
-//   .then((userCredential) => {
-//     // Signed in 
-//     var user = userCredential.user;
-//     console.log(user);
-//     // ...
-//   })
-//   .catch((error) => {
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     console.log(errorCode,errorMessage);
-//     // ..
-//   });
+let userName = document.getElementById("userName");
+let submitButton = document.getElementById("getStarted");
+let email = document.getElementById("userMail");
+let password = document.getElementById("userPassword");
+
+
+
+function creatUser() {
+  //create new user.
+  const auth = firebase.auth();
+  firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+  .then((userCredential) => {
+    // Signed in 
+    let user = userCredential.user;
+    console.log(user);
+    // ...
+  })
+
+  .catch((error) => {
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    console.log(errorCode,errorMessage);
+    alert(errorMessage);
+  // ..
+  });
+}
+
+// Checks the user input to make sure it's valid before creating new user. 
+function meetsCriteria() {
+    if (userName.value.length === 0) {
+        alert("Please enter your username!");
+    } else if (email.value.length === 0) {
+        alert("Please enter your email account!");
+    } else if (password.value.length < 8) {
+        alert("Passwords must be atleast 8 characters long!");
+    } else {
+        creatUser();
+    } 
+}
+submitButton.addEventListener("click", meetsCriteria);
