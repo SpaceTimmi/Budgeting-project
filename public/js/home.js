850b0d10441739fd3fd94a4993e96a65595e8253
vy-b <kvy.bui@gmail.com>
2022-09-07 21:40:39 -0700

    temp

--- public/js/home.js
+++ public/js/home.js
@@ -9,29 +9,7 @@ const dbRef = firebase.database().ref();
 const usersRef = dbRef.child("users");
 
 // sample write data
-// start test to list users
-const userListUI = document.getElementById("userList");
-usersRef.on("child_added", (snap) => {
-  let user = snap.val();
-  let $li = document.createElement("li");
-  $li.innerHTML = user.displayName;
-  $li.setAttribute("child-key", snap.key);
-  $li.addEventListener("click", userClicked);
-  userListUI.append($li);
-});
 
-function userClicked(e) {
-  var userID = e.target.getAttribute("child-key");
-  const userRef = dbRef.child("users/" + userID);
-  const userDetailUI = document.getElementById("userDetail");
-  userDetailUI.innerHTML = "";
-  userRef.on("child_added", (snap) => {
-    var $p = document.createElement("p");
-    $p.innerHTML = snap.key + " - " + snap.val();
-    userDetailUI.append($p);
-  });
-}
-// end test to list user
 
 // start test write to db
 function writeToDb() {
@@ -77,6 +55,30 @@ input.addEventListener("keypress", addListAfterKeypress);
 */
 
 /*======================================================*/
+// start test to list users
+const userListUI = document.getElementById("userList");
+usersRef.on("child_added", (snap) => {
+  let user = snap.val();
+  let $li = document.createElement("li");
+  $li.innerHTML = user.displayName;
+  $li.setAttribute("child-key", snap.key);
+  $li.addEventListener("click", userClicked);
+  userListUI.append($li);
+});
+
+function userClicked(e) {
+  var userID = e.target.getAttribute("child-key");
+  const userRef = dbRef.child("users/" + userID);
+  const userDetailUI = document.getElementById("userDetail");
+  userDetailUI.innerHTML = "";
+  userRef.on("child_added", (snap) => {
+    var $p = document.createElement("p");
+    $p.innerHTML = snap.key + " - " + snap.val();
+    userDetailUI.append($p);
+  });
+}
+// end test to list user
+
 function openForm() {
   document.getElementById("entry-form").style.display = "block";
 }
