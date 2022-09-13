1836068d5ea149c88676cf5a4969a2215d3109fc
vy-b <kvy.bui@gmail.com>
2022-09-13 01:00:27 -0700

    redesigned db, styling changes to homepage

--- public/js/home.js
+++ public/js/home.js
@@ -4,55 +4,58 @@ const cardList = document.getElementById("card-list");
 
 // start test to list users
 firebase.auth().onAuthStateChanged((user) => {
-  const entriesRef = firebase.database().ref("budget_entry/"+user.uid);
-  console.log(entriesRef)
-  const entryListUI = document.getElementById("entryList");
-  entriesRef.on("child_added", snap => {
-    let entry = snap.val();
-    let $entrycard = document.createElement("div");
-    var date = new Date(entry.date);
-    $entrycard.innerHTML = date.toLocaleDateString() + " " + entry.type + " category: " + entry.category + " desc: " + entry.description + " amount: " + entry.amount;
-    $entrycard.setAttribute("child-key", snap.key);
-    
-    $entrycard.addEventListener("click", entryClicked) 
-    entryListUI.append($entrycard);
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().toLocaleString("default", { month: "long" });
+  const entriesRef = firebase
+    .database()
+    .ref("budget_entry/" + user.uid + "/" + currentYear); // gets all entries for the current year
+  // first look into all the entries in the current year created by logged in user
+  // to implement: navigate to last year, next year, etc.
+  entriesRef.get().then((snap) => {
+    const header = document.getElementById("year-header");
+    header.innerText = snap.key;
+    // then look into entries in current month
+    // to implement: navigate to last month, next month, etc.
+    const entriesByMonth = firebase
+      .database()
+      .ref("budget_entry/" + user.uid + "/" + currentYear + "/" + currentMonth);
+    entriesByMonth.on("child_added", (snap) => {
+      const month = document.getElementById("month-header");
+      month.textContent = currentMonth;
+      const currentDate = snap.key;
+      let dateCard = document.createElement("div");
+      dateCard.setAttribute("class", "info-container");
+      let cardTitle = document.createElement("div");
+      cardTitle.innerHTML = `<div class="card-title">${currentDate}</div>`;
+      dateCard.append(cardTitle);
+      const entriesByDate = firebase.database().ref("budget_entry/" + user.uid + "/" + currentYear + "/" + currentMonth + "/" + snap.key);
+      entriesByDate.on("child_added", (snap) => {
+        let entry = snap.val();
+        console.log(entry);
+        /* Testing */
 
-    /* Testing */
-    
-    let cardElement = document.createElement("li");
-    
-    let cardDate = date.toLocaleDateString();
-    let cardType =  entry.type; 
-    let cardCategory = entry.category
-    let cardDescription = entry.description
-    let cardAmount = entry.amount;
+        let cardElement = document.createElement("li");
 
-    let cardC = `<div class="info-container">
-                    <p id="info">Due date: ${cardDate}</p>
-                    <p id="info">Type: ${cardType}</p>
-                    <p id="info">Category: ${cardCategory}</p>
-                    <p id="info">Amout: ${cardAmount}</p>
-                    <p id="info"> Description: ${cardDescription}</p>
-                  </div>`
-    cardElement.innerHTML = cardC;
-    cardList.append(cardElement);
-    
-    /* Testing */
-});
+        let cardType = entry.type;
+        let cardCategory = entry.category;
+        let cardDescription = entry.description;
+        let cardAmount = entry.amount;
 
-function entryClicked(e) {
-  var entryId = e.target.getAttribute("child-key");
-  const entryRef = entriesRef.child(entryId);
-  const $entryDesc = document.createElement("div");
-  userDetailUI.innerHTML = "";
-  userRef.on("child_added", (snap) => {
-    var $p = document.createElement("p");
-    $p.innerHTML = snap.key + " - " + snap.val();
-    userDetailUI.append($p);
+        let cardC = `<div class="inner-info">
+                        <p id="info">Type: ${cardType}</p>
+                        <p id="info">Category: ${cardCategory}</p>
+                        <p id="info">Amount: ${cardAmount}</p>
+                        <p id="info"> Description: ${cardDescription}</p>
+                      </div>`;
+        cardElement.innerHTML = cardC;
+        dateCard.append(cardElement);
+        cardList.append(dateCard);
+
+        /* Testing */
+      });
+    });
   });
-}
-})
-
+});
 
 function openForm() {
   document.getElementById("entry-form").style.display = "block";
@@ -61,8 +64,12 @@ function openForm() {
 function closeForm() {
   document.getElementById("entry-form").style.display = "none";
 }
-// Wrting to budget entry 
+// Wrting to budget entry
 // Getting Inputs
+
+const entryDate = document.getElementById("entry-date");
+entryDate.valueAsDate = new Date(new Date().toLocaleDateString()); // creates Date object in local time, set default value
+
 const amount = document.getElementById("amount");
 const category = document.getElementById("category");
 const description = document.getElementById("description");
@@ -71,14 +78,19 @@ const submitBtn = document.getElementById("add-btn");
 
 function writeToBudgetEntry() {
   // When called this function writes to budget_entry.
-  let today = new Date();
+
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-      let budgetRef = firebase.database().ref("budget_entry/" + user.uid);
+      const yearMonth = getYearMonth(entryDate.value); // such as "2022/Sep"
+      let budgetRef = firebase
+        .database()
+        .ref(
+          "budget_entry/" + user.uid + "/" + yearMonth + "/" + entryDate.value
+        );
       budgetRef.push().set({
         amount: amount.value,
         category: category.value,
-        date: today.toISOString(),
+        date: entryDate.value,
         description: description.value,
         type: type.value,
       });
@@ -86,42 +98,46 @@ function writeToBudgetEntry() {
       amount.value = "";
       category.value = "";
       description.value = "";
-      type.value = "";  
-    } 
+      type.value = "";
+    }
   });
-
 }
 
 //Event Listeners.
 
 submitBtn.addEventListener("click", () => {
-  // Event listener for the submit button 
+  // Event listener for the submit button
   // If the submit button is clicked and all inputs have been filled then it submits inputs to firebase.
   if (verifyInputs()) {
     writeToBudgetEntry();
     closeForm();
   } else {
-    alert("Please fill the form properly before clicking submit.")
+    alert("Please fill the form properly before clicking submit.");
   }
 });
 
-document.querySelectorAll(".entryInput").forEach(item => {
-  // Event listner for input tags. 
+document.querySelectorAll(".entryInput").forEach((item) => {
+  // Event listner for input tags.
   // If the return/enter (while cursor is on the input) key is pressed and all inputs have been filled then it submits inputs to firebase.
   item.addEventListener("keypress", (event) => {
-    if(verifyInputs() && event.which === 13) {
+    if (verifyInputs() && event.which === 13) {
       writeToBudgetEntry();
     }
   });
-})
-
+});
 
 //Helper function
 function verifyInputs() {
   // Returns true if all inputs have been filled, false otherwise.
   let arr = [amount, category, description, type];
   let check = arr.reduce((acc, input) => {
-      return acc && (input && input.value);
+    return acc && input && input.value;
   }, true);
-  return check 
+  return check;
+}
+
+function getYearMonth(date) {
+  const year = new Date(date).getFullYear();
+  const month = new Date(date).toLocaleString("default", { month: "long" });
+  return year + "/" + month;
 }
